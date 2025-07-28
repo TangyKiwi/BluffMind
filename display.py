7e5a9316dea2075f241d168c01d4978b0b607222
TangyKiwi <orange.panda.65@gmail.com>
2025-07-27 20:36:50 -0700

    Update files w/ license

--- display.py
+++ display.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 Kevin Lin
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from rich.layout import Layout
 from rich.panel import Panel
 from rich.table import Table
